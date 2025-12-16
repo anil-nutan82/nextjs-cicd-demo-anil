bf645a47fd53f03cf957c6aa7b275bd24781d20b
phoenix-anil <anilgupta@phx.co.in>
2025-12-16 18:57:44 +0530

    live add changes test

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
       <h1>🚀 Next.js CI/CD Demo</h1>
       <h1>🚀 Next.js CI/CD Demo v2</h1>
       <h1>🚀 Next.js CI/CD Demo v3</h1>
+      <h1>🚀 Next.js CI/CD Demo v4</h1>
       <p>Auto-deployed using GitHub + Vercel</p>
       <p>Last update: {new Date().toISOString()}</p>
     </main>
